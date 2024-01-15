0a1f74059f270eb1f706dbff06d059fb32b04c12
Walter <walterangelodasmarinas@gmail.com>
2024-01-15 23:36:17 +0800

    Template for the dashboard UI. Using material UI and react.

---
+++ app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import Dashboard from './Dashboard';
+
+ReactDOM.render(<Dashboard />, document.getElementById('root'));
